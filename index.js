861cef3499d901855a80ffd8084bc656de98f7aa
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2022-04-04 18:30:12 -0500

    Added files to heroku deploy

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3003;
+const port = process.env.PORT || 3003;
 
 const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('no permitido'));
