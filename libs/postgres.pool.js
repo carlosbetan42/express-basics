30bca8f4cd0e6230205618f2954dc2c7fe36c24c
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2022-04-26 17:33:44 -0500

    Antes de iniciar con sequelize

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -6,12 +6,6 @@ const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const pool = new Pool({
-  host: 'localhost',
-  port: 5437,
-  user: 'root',
-  password: 'root',
-  database: 'my_store'
-});
+const pool = new Pool({ connectionString: URI });
 
 module.exports = pool;
